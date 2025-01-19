d6adb42d950055f1c5d3a498af701e6b855e2418
Dipu Ranjan <dipuranjanec08@gmail.com>
2025-01-19 12:02:51 +0530

    formating changes & using ifstream, ofstream instead of fstream

--- add_syntax_higlighting.cpp
+++ add_syntax_higlighting.cpp
@@ -3,66 +3,46 @@
 #include <string>
 #include <vector>
 
-void test(const std::string source_file, const std::string dest_file)
-{
-    std::fstream in_stream(source_file);
-    std::fstream out_stream{dest_file, out_stream.out};
+void test(const std::string &source_file, const std::string &dest_file) {
+  std::ifstream in_stream(source_file);
+  std::ofstream out_stream(dest_file);
 
-    if (!in_stream.is_open())
-    {
-        std::cout << "failed to open " << source_file << '\n';
-        return;
-    }
+  if (!in_stream.is_open()) {
+    std::cout << "failed to open " << source_file << '\n';
+    return;
+  }
 
-    if (!out_stream.is_open())
-    {
-        std::cout << "failed to open " << dest_file << '\n';
-        return;
-    }
+  if (!out_stream.is_open()) {
+    std::cout << "failed to open " << dest_file << '\n';
+    return;
+  }
 
-    bool is_codeblock_started{false};
-    std::string prev_line;
-    for (std::string line; std::getline(in_stream, line);)
-    {
-        bool is_codeblock = line.starts_with("    ");
-        if (is_codeblock)
-        {
-            if (!is_codeblock_started)
-            {
-                if (line.empty())
-                {
-                    out_stream << "```cpp\n";
-                }
-                else
-                {
-                    out_stream << "```cpp\n"
-                               << line.replace(0, 4, "") << '\n';
-                }
-                is_codeblock_started = true;
-            }
-            else
-            {
-                out_stream << line.replace(0, 4, "") << '\n';
-            }
+  bool is_codeblock_started{false};
+  std::string prev_line;
+  for (std::string line; std::getline(in_stream, line);) {
+    bool is_codeblock = line.starts_with("    ");
+    if (is_codeblock) {
+      if (!is_codeblock_started) {
+        if (line.empty()) {
+          out_stream << "```cpp\n";
+        } else {
+          out_stream << "```cpp\n" << line.replace(0, 4, "") << '\n';
         }
-        else
-        {
-            if (is_codeblock_started && !line.empty())
-            {
-                out_stream << "```\n"
-                           << line << '\n';
-                is_codeblock_started = false;
-            }
-            else
-            {
-                out_stream << line << '\n';
-            }
-        }
-        // prev_line = line;
+        is_codeblock_started = true;
+      } else {
+        out_stream << line.replace(0, 4, "") << '\n';
+      }
+    } else {
+      if (is_codeblock_started && !line.empty()) {
+        out_stream << "```\n" << line << '\n';
+        is_codeblock_started = false;
+      } else {
+        out_stream << line << '\n';
+      }
     }
+  }
 }
 
-int main()
-{
-    test("CppCoreGuidelines.md", "CppCoreGuidelinesWithHighlighting.md");
+int main() {
+  test("CppCoreGuidelines.md", "CppCoreGuidelinesWithHighlighting.md");
 }
